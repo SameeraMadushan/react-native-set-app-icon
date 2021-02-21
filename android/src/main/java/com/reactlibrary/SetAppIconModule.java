257fdcf88c337dc7359dc3cd2556a6d3dc16e213
Sameera <sameera@objectoneworld.com>
2021-02-22 00:42:53 +0530

    android support draft

--- android/src/main/java/com/reactlibrary/SetAppIconModule.java
+++ android/src/main/java/com/reactlibrary/SetAppIconModule.java
@@ -1,5 +1,10 @@
 package com.reactlibrary;
 
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+
+import com.facebook.react.BuildConfig;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -8,6 +13,7 @@ import com.facebook.react.bridge.Callback;
 public class SetAppIconModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private String []iconColour = {"RED", "GREEN",  "BLUE"};
 
     public SetAppIconModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -23,5 +29,25 @@ public class SetAppIconModule extends ReactContextBaseJavaModule {
     public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
         // TODO: Implement some actually useful functionality
         callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    }
+
+    @ReactMethod
+    public void changeIcon(String iconName, Callback callback) {
+        int action;
+        boolean status = false;
+        final Activity activity = getCurrentActivity();
+        PackageManager packageManager = activity.getApplicationContext().getPackageManager();
+        for (String value : iconColour) {
+            if (value.equals(iconName)) {
+                action = PackageManager.COMPONENT_ENABLED_STATE_ENABLED;
+                status = true;
+            } else
+                action = PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+
+            packageManager.setComponentEnabledSetting(
+                    new ComponentName(BuildConfig.APPLICATION_ID, value),
+                    action, PackageManager.DONT_KILL_APP);
+            callback.invoke(null, status);
+        }
     }
 }
